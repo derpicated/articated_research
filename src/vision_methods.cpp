641302508f1fe3692ca84fd39c5d1acacf88163d
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-09-27 22:36:11 +0200

    add first filters and testing to preprocessing

--- src/vision_methods.cpp
+++ src/vision_methods.cpp
@@ -1,12 +1,19 @@
 #include "vision_methods.hpp"
 
+// to greyscale
+// gaussian blur
+// edge detect
+// blob detect
+
 vision_methods::vision_methods () {
-    std::cout << "vision_methods constructor" << std::endl;
 }
 
 vision_methods::~vision_methods () {
 }
 
 cv::Mat vision_methods::preprocessing (const cv::Mat image_in) {
-    return image_in;
+    cv::Mat image_out;
+    cvtColor (image_in, image_out, CV_BGR2GRAY);
+    cv::GaussianBlur (image_out, image_out, cv::Size (21, 21), 2, 2);
+    return image_out;
 }

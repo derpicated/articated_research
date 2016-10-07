e1814646edbbd7ca6065fa29f1e41496170e9b23
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-10-07 16:08:57 +0200

    update acquisition demo

--- tests/acquisition/demo.cpp
+++ tests/acquisition/demo.cpp
@@ -1,29 +1,26 @@
 #include "acquisition.hpp"
 
+// small demo to show some features
 int main (int argc, char const* argv[]) {
     if (argc != 4) {
-        std::cout << "Usage: acquisition_test image video cam_id" << std::endl;
+        std::cout << "Usage: acquisition_demo image video cam_id" << std::endl;
         exit (EXIT_FAILURE);
     }
     std::string input_img = argv[1];
     std::string input_vid = argv[2];
     int input_cam         = std::stoi (argv[3]);
     acquisition acquisition_test;
-    // set camera source in range
-    // set camera source out of range and catch exception
-    acquisition_test.source (acquisition_test.cam_count () - 1);
-    try {
-        acquisition_test.source (acquisition_test.cam_count ());
-    } catch (const std::range_error error) {
-        std::cout << "---------- source gave me something!" << std::endl;
-    }
-    std::cout << "cam count is: " << acquisition_test.cam_count () << std::endl;
+    std::cout << "camera count is: " << acquisition_test.cam_count () << std::endl;
+    // show image
     acquisition_test.source (input_img);
     cv::imshow ("input img", acquisition_test.capture ());
     cv::waitKey (0);
+    cv::destroyAllWindows ();
+    // show video
     acquisition_test.source (input_vid);
     cv::imshow ("input vid", acquisition_test.capture ());
     cv::waitKey (0);
+    // show camera
     acquisition_test.source (input_cam);
     cv::imshow ("input cam", acquisition_test.capture ());
     cv::waitKey (0);

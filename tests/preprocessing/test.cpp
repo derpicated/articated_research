63adb36a30bfef8b79c701d2061b51cb1ca4062c
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-10-11 11:48:31 +0200

    add preprocessing test

---
+++ tests/preprocessing/test.cpp
@@ -0,0 +1,36 @@
+#include "acquisition.hpp"
+#include "vision_methods.hpp"
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/ts/ts.hpp>
+
+#ifndef SAMPLES_DIR
+#define SAMPLES_DIR ""
+#endif
+
+CV_TEST_MAIN (".");
+
+TEST (preprocessing_image_no_fail_video, preprocessing) {
+    acquisition ac_test;
+    vision_methods vm_test;
+    cv::Mat image;
+    ASSERT_NO_THROW (ac_test.source (
+    std::string (SAMPLES_DIR) + std::string ("/3_marker_with_border.webm")))
+    << "the sample video file cannot be read";
+    ASSERT_NO_THROW (image = ac_test.capture ())
+    << "the file cannot be captured";
+    ASSERT_NO_THROW (image = vm_test.preprocessing (image))
+    << "preprocessing did oops";
+}
+
+TEST (preprocessing_image_no_fail_image, preprocessing) {
+    acquisition ac_test;
+    vision_methods vm_test;
+    cv::Mat image;
+    ASSERT_NO_THROW (ac_test.source (std::string (SAMPLES_DIR) + std::string ("/arctichare.png")))
+    << "the sample video file cannot be read";
+    ASSERT_NO_THROW (image = ac_test.capture ())
+    << "the file cannot be captured";
+    ASSERT_NO_THROW (image = vm_test.preprocessing (image))
+    << "preprocessing did oops";
+}

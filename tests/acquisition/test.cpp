525aa1f17c8ce8450fdb2db93385ffd3243e5ec6
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-10-11 11:14:49 +0200

    add messages to tests

--- tests/acquisition/test.cpp
+++ tests/acquisition/test.cpp
@@ -9,35 +9,39 @@ CV_TEST_MAIN (".");
 
 TEST (acquisition_cam_count, acquisition) {
     acquisition ac_test;
-    ASSERT_EQ (ac_test.cam_count (), 10);
+    ASSERT_EQ (ac_test.cam_count (), 10) << "camera count is wrong";
 }
 
 TEST (acquisition_file_fail, acquisition) {
     acquisition ac_test;
-    ASSERT_THROW (ac_test.source ("./random_file"), std::ifstream::failure);
+    ASSERT_THROW (ac_test.source ("./random_file"), std::ifstream::failure)
+    << "a random file that doesn't exist should throw an error";
 }
 
 TEST (acquisition_file_no_fail_video, acquisition) {
     acquisition ac_test;
     ASSERT_NO_THROW (ac_test.source (
-    std::string (SAMPLES_DIR) + std::string ("/3_marker_with_border.webm")));
+    std::string (SAMPLES_DIR) + std::string ("/3_marker_with_border.webm")))
+    << "the sample video file cannot be read";
 }
 
 TEST (acquisition_file_no_fail_image, acquisition) {
     acquisition ac_test;
-    ASSERT_NO_THROW (ac_test.source (
-    std::string (SAMPLES_DIR) + std::string ("/arctichare.png")));
+    ASSERT_NO_THROW (ac_test.source (std::string (SAMPLES_DIR) + std::string ("/arctichare.png")))
+    << "the sample image file cannot be read";
 }
 
 TEST (acquisition_capture_file_set, acquisition) {
     acquisition ac_test;
-    ASSERT_NO_THROW (ac_test.source (
-    std::string (SAMPLES_DIR) + std::string ("/arctichare.png")));
-    ASSERT_NO_THROW (ac_test.capture ());
+    ASSERT_NO_THROW (ac_test.source (std::string (SAMPLES_DIR) + std::string ("/arctichare.png")))
+    << "the sample file cannot be read";
+    ASSERT_NO_THROW (ac_test.capture ()) << "the file cannot be captured";
 }
 
 TEST (acquisition_capture_file_wrong_set, acquisition) {
     acquisition ac_test;
-    ASSERT_THROW (ac_test.source ("./random_file"), std::ifstream::failure);
-    ASSERT_THROW (ac_test.capture (), std::runtime_error);
+    ASSERT_THROW (ac_test.source ("./random_file"), std::ifstream::failure)
+    << "the file does not exists, so an error should be thrown";
+    ASSERT_THROW (ac_test.capture (), std::runtime_error)
+    << "the file is not set correctly, so an error should be thrown";
 }

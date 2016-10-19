3b8e6ace3223f90b6cddb41a91fb6ccc142784ff
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-10-20 01:16:05 +0200

    move compiler flags

--- cmake/acquisition.cmake
+++ cmake/acquisition.cmake
@@ -8,3 +8,5 @@ set(ACQUISITION_SRC
 add_library( acquisition SHARED ${ACQUISITION_SRC} ${ACQUISITION_INC} )
 target_link_libraries( acquisition ${OpenCV_LIBS} )
 target_include_directories ( acquisition PUBLIC ${INCLUDE_DIR} )
+
+target_compile_options(acquisition PRIVATE -std=c++11 -Wall -Wextra)

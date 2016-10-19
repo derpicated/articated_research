89646fb26c93e39120b1e2cc09e73cd1bc7e3c7a
Menno van der Graaf <mennovandergraaf@hotmail.com>
2016-10-19 02:20:46 +0200

    remove old commented code

--- src/vision_methods.cpp
+++ src/vision_methods.cpp
@@ -103,38 +103,3 @@ std::map<unsigned int, cv::Point2f>& markers) {
         }
     }
 }
-
-/*    std::vector<cv::Vec4i> hierarchy;
-    cv::RNG rng;
-    cv::findContours (image_in, contours, hierarchy, CV_RETR_TREE,
- CV_CHAIN_APPROX_SIMPLE);
-
-  cv::Mat drawing = cv::Mat::zeros (image_in.size (), CV_8UC3);
-  for (unsigned int i = 0; i < contours.size (); i++) {
-      cv::Scalar color =
-      cv::Scalar (rng.uniform (0, 255), rng.uniform (0, 255), rng.uniform
- (0, 255));
-      cv::drawContours (drawing, contours, i, color, 2, 8, hierarchy, 0);
-  }
-
-  return drawing;*/
-
-/*
-  // blob detection creation
-    cv::SimpleBlobDetector::Params blob_detector_params;
-    blob_detector_params.filterByArea        = true;
-    blob_detector_params.maxArea             = 10000.0;
-    blob_detector_params.minArea             = 10.0;
-    blob_detector_params.minDistBetweenBlobs = 5.0; // 50?
-    cv::SimpleBlobDetector blob_detector (blob_detector_params);
-
-    blob_detector.detect (image_in, key_points);
-
-    int i = 0;
-    for (cv::KeyPoint point : key_points) {
-        markers[i] = point;
-        ++i;
-    }
-
-    cv::drawKeypoints (image_in, key_points, image_out);
-*/

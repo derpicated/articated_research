58863f3f5fbe8bf9cca9cb065a469d5e63e7b409
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-10-20 20:50:00 +0200

    add small demo for decomposeHomographyMat

--- tests/classification/demo.cpp
+++ tests/classification/demo.cpp
@@ -10,30 +10,83 @@
 #define SAMPLES_DIR ""
 #endif
 
+// source ...
+// <http://stackoverflow.com/questions/35942095/opencv-strange-rotation-and-translation-matrices-from-decomposehomographymat>
+
 int main () {
     std::cout << "classification demo" << std::endl;
-    // findHomography(InputArray srcPoints, InputArray dstPoints, int method=0,
-    // double ransacReprojThreshold=3, OutputArray mask=noArray() )
-    // pt(10, 20)
-    std::vector<cv::Point2f> input = { cv::Point2f (91, 81),
-        cv::Point2f (409, 81), cv::Point2f (91, 376), cv::Point2f (409, 376) };
-    std::vector<cv::Point2f> destination = { cv::Point2f (88, 154),
-        cv::Point2f (357, 201), cv::Point2f (102, 432), cv::Point2f (401, 449) };
-    cv::Mat output;
-    output = cv::findHomography (input, destination);
-    std::cout << output << std::endl;
 
-
+    // read sample image
     cv::Mat image = cv::imread (std::string (SAMPLES_DIR) +
     std::string ("/perspective_transform_straight.png"));
     cv::Size size (image.size ());
     cv::Mat output_image (size, 0);
-    cv::warpPerspective (image, output_image, output, size);
 
+    // set up a virtual camera
+    float f = 100, w = 500, h = 500;
+
+    // |fx 0  cx| (cx, cy) is a principal point
+    // |0  fy cy|          that is usually at the image center
+    // |0  0  1 | (fx, fy) are the focal lengths expressed in pixel units.
+    cv::Mat1f K = (cv::Mat1f (3, 3) << f, 0, w / 2, 0, f, h / 2, 0, 0, 1);
+
+    // set transformation from 1st to 2nd camera (assume K is unchanged)
+    // cv::Mat1f rvecDeg = (cv::Mat1f (3, 1) << 45, 0, 0);
+    // cv::Mat1f t       = (cv::Mat1f (3, 1) << 0, 0, 0);
+
+    // known points on reference {x, y}
+    // X   X
+    //
+    // X   X
+    std::vector<cv::Point2f> input = { { 0, 0 }, { 4, 0 }, { 0, 4 }, { 4, 4 } };
+    // destination
+    // X   X
+    // X   X
+    std::vector<cv::Point2f> dest = { { 0, 0 }, { 4, 0 }, { 0, 2.828427125 },
+        { 4, 2.828427125 } };
+    // somewhat of an 45 degrees angle
+
+    // find homography beteen images
+    cv::Mat H = cv::findHomography (input, dest);
+
+    std::cout << "-------------------------------------------" << std::endl;
+    std::cout << "Estimated H = \n" << H << std::endl << std::endl;
+
+    // decompose and find information that is in the transformation matrix
+    std::vector<cv::Mat> rotations, translations, normals;
+    cv::decomposeHomographyMat (H, K, rotations, translations, normals);
+
+    std::cout << "-------------------------------------------" << std::endl;
+    std::cout << "Estimated decomposition:" << std::endl;
+    std::cout << "rotations (cv::Rodrigues)= " << std::endl;
+    for (auto r : rotations) {
+        cv::Mat1d rvec;
+        cv::Rodrigues (r, rvec);
+        std::cout << rvec * 180 / CV_PI << std::endl << std::endl;
+    }
+
+    std::cout << "rotations = " << std::endl;
+    for (auto r : rotations) {
+        std::cout << r << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "translations = " << std::endl;
+    for (auto t : translations) {
+        std::cout << t << std::endl << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "normals = " << std::endl;
+    for (auto n : normals) {
+        std::cout << n << std::endl << std::endl;
+    }
+
+    // transform and display image * H = output in 2D
+    cv::warpPerspective (image, output_image, H, size);
     cv::imshow ("outputImage", output_image);
-
-    cv::resize (output, output, { 100, 100 });
-    cv::imshow ("output img", output);
     cv::waitKey (0);
     return EXIT_SUCCESS;
 }
